19f5ef29ecb9e54760e5f0fd41ef09b7e8c74f10
asifhjahid <dhrubo8211diu@gmail.com>
2021-09-09 05:44:07 +0600

    completed item

--- js/script.js
+++ js/script.js
@@ -2,7 +2,8 @@ let inputText = document.querySelector('#input-text');
 
 let addCourse = document.querySelector('#button');
 let enrollCoursesUi = document.querySelector('.enroll-courses-list');
-
+let enrollList = enrollCoursesUi.querySelector('.item');
+let completedUiList = document.querySelector('.completed-courses-list')
 
 let updatedCourse = addCourse.addEventListener('click',function(e){
         
@@ -37,9 +38,33 @@ function addEnrollCourses(e){
 
 function completedItem(){
     let completeCourseItem = this.parentNode;
-    console.log(completeCourse);
     let deleteButton = document.createElement('button');
     deleteButton.innerText = 'Delete';
     deleteButton.className = 'deleteBtn'
     completeCourseItem.appendChild(deleteButton)
+    console.log(completeCourseItem);
+
+    let checkBox = completeCourseItem.querySelector("input[type='checkbox']");
+    checkBox.remove();
+    completedUiList.appendChild(completeCourseItem);
+    completedButton(completeCourseItem,deleteElement)
 }
+
+function deleteElement(){
+    let completeLiItem = this.parentNode;
+    console.log(completeLiItem);
+    let ulItems = completeLiItem.parentNode;
+    console.log(ulItems);
+    ulItems.removeChild(completeLiItem);
+}
+function updatedItems(completeItem,chcekboxClick){
+   let checkBox = completeItem.querySelector("input[type='checkbox']");
+   checkBox.onchange = chcekboxClick;
+    
+}
+
+function completedButton(completeItem,deleteButton){
+        let deleteButtonItem = completeItem.querySelector('.deleteBtn');
+        deleteButtonItem.onclick = deleteButton;
+}
+
